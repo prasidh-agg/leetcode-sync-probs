d6ffa5ef504771a1b11b57eb7430aed95989f631
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2023-08-14 02:04:34 +0000

    [LeetCode Sync] Runtime - 8 ms (88.76%), Memory - 45.6 MB (99.75%)

---
+++ my-folder/0056-merge-intervals/solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+
+        if(intervals.length < 2) return intervals;
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        List<int[]> result = new ArrayList<>();
+
+        for(int i = 1; i < intervals.length; i++){
+            int[] left = intervals[i-1];
+            int[] right = intervals[i];
+            if(right[0] > left[1]){
+                result.add(left);
+            }
+            else{
+                right[0] = left[0];
+                if(left[1] > right[1]){
+                    right[1] = left[1];
+                }
+            }
+            if(i == intervals.length - 1){
+                result.add(right);
+            }
+        }
+
+        int[][] array = new int[result.size()][2];
+        for (int i = 0; i < result.size(); i++) {
+            array[i] = result.get(i);
+        }
+
+        return array;
+
+    }
+}
