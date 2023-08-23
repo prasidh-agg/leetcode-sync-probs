0d5197cf999b6d5c14303e0bd8c83f41e08f0f43
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2023-08-23 19:26:52 +0000

    [LeetCode Sync] Runtime - 2 ms (80.68%), Memory - 43.2 MB (99.98%)

---
+++ my-folder/0023-merge-k-sorted-lists/solution.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists == null || lists.length == 0) return null;
+        int step = 1, k = lists.length;
+        while(step < k){
+            for(int i = 0; i < k - step; i += 2*step){
+                lists[i] = mergeTwoLists(lists[i], lists[i+step]);
+            }
+            step *= 2;
+        }
+        return lists[0];
+    }
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2){
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        if(l1.val < l2.val){
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+        else{
+            l2.next = mergeTwoLists(l1,l2.next);
+            return l2;
+        }
+    }
+}
