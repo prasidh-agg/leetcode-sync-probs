a144b9b0d85b54805e732c144932eb4cc2c3800d
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2023-08-29 21:07:03 +0000

    [LeetCode Sync] Runtime - 1 ms (99.96%), Memory - 44.3 MB (99.76%)

--- my-folder/0055-jump-game/solution.java
+++ my-folder/0055-jump-game/solution.java
@@ -1,13 +1,11 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int goal = nums.length - 1;
-
         for(int i = nums.length - 2; i >= 0; i--){
-            if ((nums[i] + i) >= goal){
+            if((nums[i] + i) >= goal){
                 goal = i;
             }
         }
-
         return goal == 0;
     }
 }
