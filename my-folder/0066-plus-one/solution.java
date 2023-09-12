ab60c99c6a7c39be168b65fc6dbcd24d19d3553b
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2023-09-12 22:15:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.7 MB (99.94%)

---
+++ my-folder/0066-plus-one/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+
+        // add 1 from the back.. if no carry.. return the answer..
+        int carry = 0;
+
+        for(int i = digits.length - 1; i >= 0; i--){
+            if(digits[i] < 9){
+                digits[i] += 1;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] res = new int[digits.length + 1];
+        res[0] = 1;
+        for(int i = 0; i < digits.length; i++){
+            res[i+1] = digits[i];
+        }
+        return res;
+    }
+}
