3b6c4c9b9ab70b5d17bace084e97efc6925c855a
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2023-09-20 19:29:21 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.4 MB (100.00%)

---
+++ my-folder/0543-diameter-of-binary-tree/solution.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int maxDiameter = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        calcDiameter(root);
+        return maxDiameter;
+    }
+
+    private int calcDiameter(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int leftDepth = calcDiameter(root.left);
+        int rightDepth = calcDiameter(root.right);
+        maxDiameter = Math.max(maxDiameter, leftDepth + rightDepth);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
