077c3141532a34a27daa699f845284e25530807c
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2023-09-20 20:18:01 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 39.9 MB (92.39%)

--- my-folder/0070-climbing-stairs/solution.java
+++ my-folder/0070-climbing-stairs/solution.java
@@ -1,14 +1,13 @@
 class Solution {
-
     public int climbStairs(int n) {
-       if(n < 2) return 1;
-       int prev = 1, prev2 = 1;
+        if(n < 2) return 1;
+        int prev = 1, prev2 = 1;
 
-       for(int i = 2; i <= n; i++){
-           int curr = prev + prev2;
-           prev2 = prev;
-           prev = curr;
-       }
-       return prev;
+        for(int i = 2; i <= n; i++){
+            int curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
     }
 }
