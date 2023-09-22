fcd05bfc0ee4b7fc1ce0cecc4afd9db7ef6e3708
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2023-09-22 17:22:34 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40 MB (99.99%)

--- my-folder/0392-is-subsequence/solution.java
+++ my-folder/0392-is-subsequence/solution.java
@@ -1,14 +1,18 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
+        int m = s.length(), n = t.length();
+        if(m > n) return false;
+        char[] sarr = s.toCharArray();
+        char[] tarr = t.toCharArray();
+
         int p1 = 0, p2 = 0;
-        while(p1 < s.length() && p2 < t.length()){
-            if(s.charAt(p1) == t.charAt(p2)){
-                p1++; p2++;
+
+        while(p1 < m && p2 < n){
+            if(sarr[p1] == tarr[p2]){
+                p1++;
             }
-            else{
-                p2++;
-            }
+            p2++;
         }
-        return p1 == s.length();
+        return p1 == m;
     }
 }
