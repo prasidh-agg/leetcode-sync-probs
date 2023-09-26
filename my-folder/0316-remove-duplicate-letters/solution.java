7ee7c80a158896fd46e75760dc19a3e884edb535
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2023-09-26 19:31:10 +0000

    [LeetCode Sync] Runtime - 2 ms (96.62%), Memory - 40.9 MB (100.00%)

---
+++ my-folder/0316-remove-duplicate-letters/solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+
+        Stack<Character> st = new Stack<>();
+        int[] count = new int[26];
+        boolean[] visited = new boolean[26];
+
+        //build a frequency array
+        for(char c : s.toCharArray()){
+            count[c - 'a']++;
+        }
+
+        for(char c : s.toCharArray()){
+            count[c - 'a']--;
+
+            if(visited[c - 'a']) continue;
+
+            while(!st.isEmpty() && st.peek() > c && count[st.peek() - 'a'] > 0){
+                visited[st.peek() - 'a'] = false;
+                st.pop();
+            }
+            st.push(c);  
+            visited[c - 'a'] = true;
+        }    
+
+        StringBuilder sb = new StringBuilder();
+        while(!st.isEmpty()){
+            char ch = st.pop();
+            sb.insert(0, ch);
+        }
+        return sb.toString();
+    }
+}
