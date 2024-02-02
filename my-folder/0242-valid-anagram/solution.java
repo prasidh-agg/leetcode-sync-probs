5d7be24948df155b1f6d57f71a4ab8cb49eb4730
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2024-02-02 22:31:47 +0000

    [LeetCode Sync] Runtime - 3 ms (89.85%), Memory - 43.2 MB (69.73%)

--- my-folder/0242-valid-anagram/solution.java
+++ my-folder/0242-valid-anagram/solution.java
@@ -1,16 +1,19 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length())return false;
 
-        int[] count = new int[256];
+        if(s.length() != t.length()) return false;
+        
+        int[] count = new int[26];
 
         for(char c : s.toCharArray()){
-            count[c] += 1;
+            count[c - 'a']++;
         }
+
         for(char c : t.toCharArray()){
-            if(count[c] == 0) return false;
-            count[c]--;
+            if(count[c - 'a'] <= 0) return false;
+            count[c - 'a']--;
         }
+
         return true;
     }
 }
