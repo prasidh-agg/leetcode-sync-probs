1ccc7c2519bc8d360fe61c0c98c3a66f3a38b39c
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2024-02-12 21:23:12 +0000

    [LeetCode Sync] Runtime - 6 ms (83.90%), Memory - 45.3 MB (15.20%)

--- my-folder/0387-first-unique-character-in-a-string/solution.java
+++ my-folder/0387-first-unique-character-in-a-string/solution.java
@@ -1,12 +1,12 @@
 class Solution {
     public int firstUniqChar(String s) {
-       for(char c : s.toCharArray()){
-            int index = s.indexOf(c);
-            int lastIndex = s.lastIndexOf(c);
-            
-            if(index == lastIndex)
-                return index;
-            }
+        int[] arr = new int[26];
+        for(char c : s.toCharArray()){
+            arr[c - 'a']++;
+        }
+        for(int i = 0; i < s.length(); i++){
+            if(arr[s.charAt(i) - 'a'] == 1) return i; 
+        }
         return -1;
     }
 }
