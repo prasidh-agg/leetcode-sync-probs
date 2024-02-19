543269f07b10f2fde304627e2f9439563b35a570
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2024-02-19 20:12:08 +0000

    [LeetCode Sync] Runtime - 146 ms (53.34%), Memory - 41.8 MB (22.69%)

--- my-folder/0079-word-search/solution.java
+++ my-folder/0079-word-search/solution.java
@@ -3,31 +3,32 @@ class Solution {
         int m = board.length;
         int n = board[0].length;
         boolean[][] visited = new boolean[m][n];
-
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(board[i][j] == word.charAt(0)){
-                    if(search(board, word, visited, i, j, 0)){
-                        return true;
-                    }
+                if(board[i][j] == word.charAt(0) && find(board, word, visited, i, j, 0)){
+                    return true;
                 }
             }
         }
         return false;
     }
-        public boolean search(char[][] board, String word, boolean[][] visited, int i, int j, int wordLen){
-            if(wordLen == word.length()){
-                return true;
-            }
-            if(i < 0 || i >= board.length || j < 0 || j >= board[0].length){
-                return false;
-            }
-            if(visited[i][j] || board[i][j] != word.charAt(wordLen)){
-                return false;
-            }
-            visited[i][j] = true;
-            boolean result = search(board, word, visited, i - 1, j, wordLen + 1) || search(board, word, visited, i + 1, j, wordLen + 1) || search(board, word, visited, i , j - 1, wordLen + 1) || search(board, word, visited, i, j + 1, wordLen + 1);
-            visited[i][j] = false;
-            return result;
-        }
+
+    public boolean find(char[][] board, String word, boolean[][] visited, int i, int j, int wordLen){
+
+        //entire word found
+        if(wordLen == word.length()) return true;
+
+        //out of board bounds
+        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length) return false;
+
+        if(visited[i][j] || word.charAt(wordLen) != board[i][j]) return false;
+
+        visited[i][j] = true;
+        boolean result = find(board, word, visited, i - 1, j, wordLen + 1) || 
+                        find(board, word, visited, i + 1, j, wordLen + 1) ||
+                        find(board, word, visited, i , j - 1, wordLen + 1) || 
+                        find(board, word, visited, i, j + 1, wordLen + 1);
+        visited[i][j] = false;
+        return result;
+    }
 }
