bde2c3e7cf3f30afb4969678f4e93b14612ce12a
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2024-02-20 21:08:31 +0000

    [LeetCode Sync] Runtime - 1 ms (95.73%), Memory - 45.9 MB (8.43%)

--- my-folder/0605-can-place-flowers/solution.java
+++ my-folder/0605-can-place-flowers/solution.java
@@ -5,9 +5,9 @@ class Solution {
         
         for(int i = 0; i < flowerbed.length; i++){
             if(flowerbed[i] == 0){
-                int prev = (i == 0 || flowerbed[i-1] == 0) ? 0 : 1;
-                int next = (i == flowerbed.length - 1 || flowerbed[i+1] == 0) ? 0 : 1;
-                if(prev == 0 && next == 0){
+                boolean prev = (i == 0 || flowerbed[i-1] == 0);
+                boolean next = (i == flowerbed.length - 1 || flowerbed[i+1] == 0);
+                if(prev && next){
                     flowerbed[i] = 1;
                     n--;
                 }
@@ -16,4 +16,3 @@ class Solution {
         return n <= 0;
     }
 }
-
