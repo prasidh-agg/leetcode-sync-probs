5d3a54ea5e9c158699730215fd1ec827c5b48fa7
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2024-02-20 21:24:56 +0000

    [LeetCode Sync] Runtime - 2 ms (94.16%), Memory - 134.1 MB (25.25%)

--- my-folder/0334-increasing-triplet-subsequence/solution.java
+++ my-folder/0334-increasing-triplet-subsequence/solution.java
@@ -1,22 +1,13 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-
-        if(nums.length < 3) return false;
-
-        int first = Integer.MAX_VALUE;  
+        int first = Integer.MAX_VALUE;
         int second = Integer.MAX_VALUE;
-        
-        for (int num : nums) {
-            if (num <= first) {
-                first = num;
-            } else if (num <= second) {
-                second = num;
-            } else {
-                return true;
-            }
+
+        for(int num : nums){
+            if(num <= first) first = num;
+            else if(num <= second) second = num;
+            else return true;
         }
-        System.gc();
         return false;
     }
 }
-
