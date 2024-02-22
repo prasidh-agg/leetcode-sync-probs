0e29aad6a5f4e76e4ccc38ed9e056c84655e9a98
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2024-02-22 05:35:54 +0000

    [LeetCode Sync] Runtime - 3 ms (82.59%), Memory - 47 MB (40.77%)

--- my-folder/1046-max-consecutive-ones-iii/solution.java
+++ my-folder/1046-max-consecutive-ones-iii/solution.java
@@ -1,26 +1,21 @@
-public class Solution {
+class Solution {
     public int longestOnes(int[] nums, int k) {
-        int maxConsecutiveOnes = 0;
-        int zeroCount = 0;
-        int left = 0;
+        int left = 0, maxOnes = 0, zeroCount = 0;
 
-        for (int right = 0; right < nums.length; right++) {
-            if (nums[right] == 0) {
-                zeroCount++;
-            }
-
-            while (zeroCount > k) {
-                if (nums[left] == 0) {
+        for(int right = 0; right < nums.length; right++){
+            if(nums[right] == 0) zeroCount++;
+            
+            //Shrink the window from left because we have way too many zeroes in our current window
+            while(zeroCount > k){
+                if(nums[left] == 0){
                     zeroCount--;
                 }
                 left++;
             }
 
-            // Update maxConsecutiveOnes within the valid window
-            maxConsecutiveOnes = Math.max(maxConsecutiveOnes, right - left + 1);
+            maxOnes = Math.max(maxOnes, right - left + 1);
         }
 
-        return maxConsecutiveOnes;
+        return maxOnes;
     }
 }
-
