70fcbc2404573ad1efc279f9a578ee828c4fe2c3
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2024-02-23 15:43:59 +0000

    [LeetCode Sync] Runtime - 2 ms (95.06%), Memory - 46.4 MB (61.57%)

--- my-folder/0338-counting-bits/solution.java
+++ my-folder/0338-counting-bits/solution.java
@@ -1,18 +1,11 @@
 class Solution {
     public int[] countBits(int n) {
-       int[] result = new int[n + 1];
-       for(int i = 0; i <= n; i++){
-           result[i] = hammingWeight(i);
-       } 
-       return result;
-    }
-
-    public int hammingWeight(int n) {
-        int count = 0;
-        while(n != 0){
-            count++;
-            n = n & (n-1);
+        int[] ans = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            ans[i] = ans[i >> 1] + (i & 1);
         }
-        return count;
+        return ans;
     }
 }
+// a number n has the same 1 bits as it has when divided by 2(right shifting 1 bit)..
+// we also need to add the last bit, because if the number is odd then that 1 bit will be lost
