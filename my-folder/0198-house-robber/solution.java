9c00270501bbd8b7aa3710d4d4ef04cbed63e6ca
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2024-03-19 03:24:29 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.9 MB (55.44%)

--- my-folder/0198-house-robber/solution.java
+++ my-folder/0198-house-robber/solution.java
@@ -2,13 +2,10 @@ class Solution {
     public int rob(int[] nums) {
         int prev = nums[0];
         int prev2 = 0;
-
-        for(int i = 1; i < nums.length; i++){
-            int pick = nums[i];
-            if(i > 1) pick += prev2;
-
-            int notPick = prev;
-            int curr = Math.max(pick, notPick);
+        for(int i = 2; i <= nums.length; i++){
+            int rob = nums[i - 1] + prev2;
+            int notRob = prev;
+            int curr = Math.max(rob, notRob);
             prev2 = prev;
             prev = curr;
         }
