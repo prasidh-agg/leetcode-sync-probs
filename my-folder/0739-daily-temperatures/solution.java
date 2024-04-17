ae1f9064839bdc0c0ab392d1943125c5286d0d7f
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2024-04-17 18:16:55 +0000

    [LeetCode Sync] Runtime - 74 ms (38.51%), Memory - 63.5 MB (6.88%)

--- my-folder/0739-daily-temperatures/solution.java
+++ my-folder/0739-daily-temperatures/solution.java
@@ -1,24 +1,14 @@
 class Solution {
-    public int[] dailyTemperatures(int[] temperatures) {
-        Deque<Integer> st = new LinkedList<>();
-        int n = temperatures.length;
-        int[] res = new int[temperatures.length];
-
-        for(int i = n - 1; i >= 0; i--){
-
-            //remove elements that are smaller than current temp from stack
-            while(!st.isEmpty() && temperatures[i] >= temperatures[st.peek()]){
-                st.pop();
+    public int[] dailyTemperatures(int[] temps) {
+        int[] results = new int[temps.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < temps.length; i++) {
+            while (!stack.isEmpty() && temps[stack.peek()] < temps[i]) {
+                results[stack.peek()] = i - stack.pop();
             }
-
-            //if stack empty means there are no bigger temps ahead.. let the answer just be default 0 in the array
-            if(!st.isEmpty()){
-                res[i] = st.peek() - i;
-            }
-
-            st.push(i);
+            stack.push(i);
         }
-        System.gc();
-        return res;
+
+        return results;
     }
 }
