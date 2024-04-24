d8b329cb5647ddda7f453c1adc4c1724c3a2995e
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2024-04-24 14:29:44 -0700

    [directory rename]

---
+++ leetcode/1604-least-number-of-unique-integers-after-k-removals/solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int findLeastNumOfUniqueInts(int[] arr, int k) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int num : arr){
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int[] count = new int[map.size()];
+        int index = 0;
+        for(int val : map.values()){
+            count[index++] = val;
+        }
+
+        Arrays.sort(count);
+
+        for(int i = 0; i < count.length; i++){
+            k -= count[i];
+            if(k > 0) continue;
+            else if(k == 0){
+                return count.length - i - 1;
+            }
+            else{
+                return count.length - i;
+            }
+        }
+        return 0;
+    }
+}
+
+
