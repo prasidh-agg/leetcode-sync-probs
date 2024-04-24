ea1180267ef5468ba285442db46e6af604fa5e3d
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2024-04-24 18:58:08 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.2 MB (59.29%)

--- my-folder/1236-n-th-tribonacci-number/solution.java
+++ my-folder/1236-n-th-tribonacci-number/solution.java
@@ -4,7 +4,7 @@ class Solution {
         dp[1] = 1;
         dp[2] = 1;
         for(int i = 3; i <= n; i++){
-            dp[i] = dp[i-3] + dp[i-2] + dp[i-1];
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
         }
         return dp[n];
     }
