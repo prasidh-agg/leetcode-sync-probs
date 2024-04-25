4a5a59aa31596a3dd4bb3da294e8ca14c553aa47
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2024-04-25 21:12:30 +0000

    [LeetCode Sync] Runtime - 1 ms (99.66%), Memory - 61.9 MB (22.68%)

--- leetcode/0121-best-time-to-buy-and-sell-stock/solution.java
+++ leetcode/0121-best-time-to-buy-and-sell-stock/solution.java
@@ -1,14 +1,20 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        if(prices.length < 2) return 0;
+        int maxProfit = 0;
+        int leftMin = prices[0];
 
-        int min = prices[0];
-        int profit = 0;
-        for(int i = 1; i < prices.length; i++){
-            profit = Math.max(prices[i] - min, profit);
-            min = Math.min(min, prices[i]);
+        for(int price : prices) {
+
+            //check if our leftMin is still the smallest, just recalc max profit.. by finding cost of selling now
+            if(price > leftMin) {
+                maxProfit = Math.max(maxProfit, price - leftMin);
+            } 
+            // this means we found a new leftMin
+            else {
+                leftMin = price;
+            }
         }
 
-        return profit;
+        return maxProfit;
     }
 }
