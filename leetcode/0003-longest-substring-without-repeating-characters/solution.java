cc45134747737d554fb1cd571928891076567e67
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2024-04-26 07:55:23 +0000

    [LeetCode Sync] Runtime - 2 ms (98.79%), Memory - 43.4 MB (87.57%)

--- leetcode/0003-longest-substring-without-repeating-characters/solution.java
+++ leetcode/0003-longest-substring-without-repeating-characters/solution.java
@@ -1,9 +1,8 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int maxLen = 0;
         int[] arr = new int[256];
         int l = 0, r = 0;
-
+        int maxLen = 0;
         while(r < s.length()){
             char c = s.charAt(r);
             l = (arr[c] > 0) ? Math.max(arr[c], l) : l;
@@ -11,7 +10,6 @@ class Solution {
             maxLen = Math.max(maxLen, r - l + 1);
             r++;
         }
-        
         return maxLen;
     }
 }
