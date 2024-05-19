127f9a9f23ec5393ba1dd6e1214ddbd95030698a
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2024-05-19 20:33:22 +0000

    [LeetCode Sync] Runtime - 6 ms (28.05%), Memory - 41.8 MB (81.85%)

--- leetcode/0592-fraction-addition-and-subtraction/solution.java
+++ leetcode/0592-fraction-addition-and-subtraction/solution.java
@@ -1,69 +1,40 @@
-class Fraction {
-    private int num;
-    private int den;
+class Solution {
+    public String fractionAddition(String expression) {
 
-    public Fraction(int numerator, int denominator) {
-        this.num = numerator;
-        this.den = denominator;
-        reduce();
-    }
+        int num = 0, den = 1;
 
-    public void add(Fraction other) {
-        this.num = this.num * other.den + other.num * this.den;
-        this.den = this.den * other.den;
-        reduce();
-    }
-    public Fraction negate() {
-        return new Fraction(-this.num, this.den);
-    }
+        if(Character.isDigit(expression.charAt(0))){
+            expression = "+" + expression;
+        }
 
+        int i = 0, n = expression.length();
+        while(i < n){
+            int sign = expression.charAt(i) == '-' ? -1 : 1;
+            i++;
 
-    private void reduce() {
-        int gcd = gcd(Math.abs(this.num), this.den);
-        this.num /= gcd;
-        this.den /= gcd;
-    }
+            int j = i;
+            while(j < n && expression.charAt(j) != '-' && expression.charAt(j) != '+'){
+                j++;
+            }
 
-    private static int gcd(int a, int b) {
-        return b == 0 ? a : gcd(b, a % b);
-    }
+            String s = expression.substring(i, j);
+            String[] parts = s.split("/");
 
-    @Override
-    public String toString() {
+            int newNum = Integer.parseInt(parts[0]), newDen = Integer.parseInt(parts[1]);
+
+            num = num * newDen + (sign * den * newNum);
+            den = den * newDen;
+            i = j;
+        }
+
+        int gcd = getGCD(Math.abs(num), den);
+        num /= gcd;
+        den /= gcd;
+
         return num + "/" + den;
     }
 
-    public static Fraction parseFraction(String frac) {
-        String[] parts = frac.split("/");
-        int numerator = Integer.parseInt(parts[0]);
-        int denominator = Integer.parseInt(parts[1]);
-        return new Fraction(numerator, denominator);
+    private int getGCD(int a, int b){
+        return b == 0 ? a : getGCD(b, a % b);
     }
 }
-
-class Solution {
-    public String fractionAddition(String expression) {
-        if (Character.isDigit(expression.charAt(0))) {
-            expression = "+" + expression;
-        }
-        Fraction result = new Fraction(0, 1); // Initialize with 0/1
-        int i = 0, n = expression.length();
-        while (i < n) {
-            int sign = expression.charAt(i) == '-' ? -1 : 1;
-            i++;
-            int j = i;
-            while (j < n && expression.charAt(j) != '+' && expression.charAt(j) != '-') {
-                j++;
-            }
-            String s = expression.substring(i, j);
-            Fraction f = Fraction.parseFraction(s);
-            if (sign == -1) {
-                f = f.negate(); // Apply the negative sign
-            }
-            result.add(f);
-            i = j;
-        }
-        return result.toString();
-    }
-}
-
