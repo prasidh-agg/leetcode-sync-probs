bda742a4c4c7ca618045b5f238cbbc8e2aea77bb
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2024-05-19 06:51:30 +0000

    [LeetCode Sync] Runtime - 1 ms (99.72%), Memory - 45.3 MB (99.79%)

--- leetcode/0064-minimum-path-sum/solution.java
+++ leetcode/0064-minimum-path-sum/solution.java
@@ -1,24 +1,22 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-       int m = grid.length, n = grid[0].length;
-       int[][] dp = new int[m][n];
-       for(int i = 0; i < m; i++){
-           Arrays.fill(dp[i], -1);
-       }
-       return helper(m - 1, n - 1, grid, dp); 
+        int m = grid.length, n = grid[0].length;
+        int[][] dp = new int[m][n];
+        for(int[] row : dp){
+            Arrays.fill(row, - 1);
+        }
+        return helper(grid, m - 1, n - 1, dp);
     }
-    private int helper(int i, int j, int[][] grid, int[][] dp){
-        //reached top left
+    public int helper(int[][] grid, int i, int j, int[][] dp){
         if(i == 0 && j == 0) return grid[i][j];
 
-        //invalid path
-        if(i < 0 || j < 0) return Integer.MAX_VALUE;
-
+        if(i < 0 || j < 0){
+            return Integer.MAX_VALUE;
+        }
         if(dp[i][j] != -1) return dp[i][j];
 
-        int up = helper(i - 1, j, grid, dp);
-        int left = helper(i, j - 1, grid, dp);
-
-        return dp[i][j] = Math.min(up, left) + grid[i][j];
+        int top = helper(grid, i - 1, j, dp);
+        int down = helper(grid, i, j - 1, dp);
+        return dp[i][j] = grid[i][j] + Math.min(top, down);
     }
 }
