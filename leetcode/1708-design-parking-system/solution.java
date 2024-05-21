89e74c17adf95b74289b22df60785e33a1ab845e
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2024-05-21 05:43:28 +0000

    [LeetCode Sync] Runtime - 7 ms (100.00%), Memory - 45.1 MB (95.68%)

---
+++ leetcode/1708-design-parking-system/solution.java
@@ -0,0 +1,35 @@
+class ParkingSystem {
+
+    private int big;
+    private int medium;
+    private int small;
+
+    public ParkingSystem(int big, int medium, int small) {
+        this.big = big;
+        this.medium = medium;
+        this.small = small;
+        
+    }
+    
+    public boolean addCar(int carType) {
+        if(carType == 1 && this.big > 0){
+            this.big -= 1;
+            return true;
+        }
+        else if(carType == 2 && this.medium > 0){
+            this.medium -= 1;
+            return true;
+        }
+        else if(carType == 3 && this.small > 0){
+            this.small -= 1;
+            return true;
+        }
+        else return false;
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * boolean param_1 = obj.addCar(carType);
+ */
