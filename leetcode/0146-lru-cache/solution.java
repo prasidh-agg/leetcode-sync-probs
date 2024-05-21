94761756a0d92d1363873f87b0f44238943228c0
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2024-05-21 02:31:09 +0000

    [LeetCode Sync] Runtime - 47 ms (48.49%), Memory - 127.4 MB (9.53%)

--- leetcode/0146-lru-cache/solution.java
+++ leetcode/0146-lru-cache/solution.java
@@ -1,26 +1,67 @@
 class LRUCache {
 
-    private int maxKeys;
-    LinkedHashMap<Integer, Integer> map;
+    Map<Integer, Node> cache;
+    Node front;
+    Node back;
+    int cap;
+
+    class Node{
+        private int key;
+        private int val;
+        private Node next;
+        private Node prev;
+
+        public Node(int key, int val){
+            this.key = key;
+            this.val = val;
+        }
+    }
 
     public LRUCache(int capacity) {
-        this.maxKeys = capacity;
-        this.map = new LinkedHashMap<>(capacity, 0.75f, true){
-            @Override
-            protected boolean removeEldestEntry(Map.Entry<Integer,Integer> eldest) {
-                return size() > maxKeys;
-            }
-        };
+        cap = capacity;
+        cache = new HashMap<>();
+        front = new Node(-1, 0);
+        back = new Node(-1, 0);
+        front.next = back;
+        back.prev = front;
     }
     
     public int get(int key) {
-        return map.getOrDefault(key, -1);
+        if(!cache.containsKey(key)) return -1;
+        int val = cache.get(key).val;
+        remove(cache.get(key));
+        insertAtBack(cache.get(key));
+        return val;
     }
     
     public void put(int key, int value) {
-        map.put(key, value);
+        if(cache.containsKey(key)){
+            remove(cache.get(key));
+        }
+        cache.put(key, new Node(key, value));
+        insertAtBack(cache.get(key));
+
+        if(cache.size() > cap){
+            Node lru = front.next;
+            remove(lru);
+            cache.remove(lru.key);
+        }   
     }
 
+    public void remove(Node node){
+        Node next = node.next;
+        Node prev = node.prev;
+        prev.next = next;
+        next.prev = prev;
+    }
+
+    public void insertAtBack(Node node){
+        Node prev = this.back.prev;
+        prev.next = node;
+        node.prev = prev;
+        node.next = this.back;
+        this.back.prev = node;
+    }
 }
 
 /**
