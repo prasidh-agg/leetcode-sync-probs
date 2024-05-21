49de5692c092e9c9f0aa8a91dae03d37a9a73c4b
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2024-05-21 02:50:33 +0000

    [LeetCode Sync] Runtime - 14 ms (99.66%), Memory - 49.1 MB (64.15%)

--- leetcode/0817-design-hashmap/solution.java
+++ leetcode/0817-design-hashmap/solution.java
@@ -1,21 +1,66 @@
+class Node {
+    int key;
+    int val;
+    Node next;
+
+    public Node(int key, int val, Node next){
+        this.key = key;
+        this.val = val;
+        this.next = next;
+    }
+}
+
 class MyHashMap {
-    int[] arr;
+
+    final int size = 10000;
+    Node[] hashTable;
 
     public MyHashMap() {
-        arr = new int[1000001];
-        Arrays.fill(arr, -1);
+        this.hashTable = new Node[size];       
     }
-    
+
+    private int hash(int key){
+        return key % size;
+    }
+
     public void put(int key, int value) {
-        arr[key] = value;
+        remove(key);
+        int h = hash(key);
+        //set the current node sitting in hashtable, as the next of the new node
+        Node node = new Node(key, value, hashTable[h]);
+
+        //finally, make this new node, the current starting node.
+        //basically, we are adding the new node, to the start of the list
+        hashTable[h] = node;
     }
     
     public int get(int key) {
-        return arr[key];
+        int h = hash(key);
+        Node node = hashTable[h];
+
+        while(node != null){
+            if(node.key == key) return node.val;
+            node = node.next;
+        }
+        return -1;
     }
     
     public void remove(int key) {
-        arr[key] = -1;
+        int h = hash(key);
+        Node node = hashTable[h];
+        if(node == null) return;
+        //if current node is the one we want to remove,
+        //then just tell hashtable's node to move one ahead.
+        if(node.key == key) hashTable[h] = node.next;
+        else{
+            while(node.next != null){
+                if(node.next.key == key){
+                    node.next = node.next.next;
+                    return;
+                }
+                node = node.next;
+            }
+        }
     }
 }
 
