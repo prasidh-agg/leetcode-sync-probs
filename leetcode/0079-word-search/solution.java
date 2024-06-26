ffacc397590155bbb897f726c37cac307255d864
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2024-06-26 04:16:07 +0000

    [LeetCode Sync] Runtime - 145 ms (50.33%), Memory - 41.2 MB (99.26%)

--- leetcode/0079-word-search/solution.java
+++ leetcode/0079-word-search/solution.java
@@ -3,8 +3,10 @@ class Solution {
         int m = board.length;
         int n = board[0].length;
         boolean[][] visited = new boolean[m][n];
+
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
+                //if found the starting character, then do a dfs search
                 if(board[i][j] == word.charAt(0) && find(board, word, visited, i, j, 0)){
                     return true;
                 }
@@ -13,21 +15,20 @@ class Solution {
         return false;
     }
 
-    public boolean find(char[][] board, String word, boolean[][] visited, int i, int j, int wordLen){
+    private boolean find(char[][] board, String word, boolean[][] visited, int i, int j, int pos){
+        //this means entire word was found
+        if(pos == word.length()) return true;
 
-        //entire word found
-        if(wordLen == word.length()) return true;
-
-        //out of board bounds
+        //checking out of bounds
         if(i < 0 || j < 0 || i >= board.length || j >= board[0].length) return false;
 
-        if(visited[i][j] || word.charAt(wordLen) != board[i][j]) return false;
+        if(visited[i][j] || word.charAt(pos) != board[i][j]) return false;
 
         visited[i][j] = true;
-        boolean result = find(board, word, visited, i - 1, j, wordLen + 1) || 
-                        find(board, word, visited, i + 1, j, wordLen + 1) ||
-                        find(board, word, visited, i , j - 1, wordLen + 1) || 
-                        find(board, word, visited, i, j + 1, wordLen + 1);
+        boolean result = find(board, word, visited, i - 1, j, pos + 1) || 
+                        find(board, word, visited, i + 1, j, pos + 1) ||
+                        find(board, word, visited, i , j - 1, pos + 1) || 
+                        find(board, word, visited, i, j + 1, pos + 1);
         visited[i][j] = false;
         return result;
     }
