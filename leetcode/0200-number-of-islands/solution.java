cdeb8da918db0d64727d1e834c3181b8e73c69ff
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2024-06-26 04:06:42 +0000

    [LeetCode Sync] Runtime - 4 ms (49.65%), Memory - 52.1 MB (23.24%)

--- leetcode/0200-number-of-islands/solution.java
+++ leetcode/0200-number-of-islands/solution.java
@@ -1,24 +1,33 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int count = 0;
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[i].length; j++){
+        int m = grid.length;
+        int n = grid[0].length;
+        int islands = 0;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                //found an island
                 if(grid[i][j] == '1'){
-                    count++;
+                    islands++;
                     dfs(grid, i, j);
                 }
             }
         }
-        return count;
+        return islands;
     }
-    public void dfs(char[][] grid, int i, int j){
-        if(i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0'){
+
+    private void dfs(char[][] grid, int i, int j){
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0')
             return;
-        }
+        
+        //mark as visited by switching to 0
         grid[i][j] = '0';
-        dfs(grid, i+1, j);
-        dfs(grid, i-1 ,j);
-        dfs(grid, i, j+1);
-        dfs(grid, i, j-1);
+        int[][] directions = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+        for(int[] direction : directions){
+            int newI = i + direction[0];
+            int newJ = j + direction[1];
+            dfs(grid, newI, newJ);
+        }
+        return;
     }
 }
