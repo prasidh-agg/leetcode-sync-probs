13e6bc75a1455e77706bca5b521464155f7b9b26
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2024-07-01 00:39:33 +0000

    [LeetCode Sync] Runtime - 1 ms (68.64%), Memory - 46 MB (8.82%)

--- leetcode/0073-set-matrix-zeroes/solution.java
+++ leetcode/0073-set-matrix-zeroes/solution.java
@@ -3,33 +3,22 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
 
-        int col0 = 1;
+        boolean[] rows = new boolean[m];
+        boolean[] cols = new boolean[n];
 
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(matrix[i][j] == 0){
-                    if(j != 0) matrix[0][j] = 0;
-                    else col0 = 0;
-                    matrix[i][0] = 0;
+                    rows[i] = true;
+                    cols[j] = true;
                 }
             }
         }
-        for(int i = 1; i < m; i++){
-            for(int j = 1; j < n; j++){
-                if(matrix[i][j] != 0){
-                    if(matrix[0][j] == 0 || matrix[i][0] == 0) matrix[i][j] = 0;
-                }
-            }
-        }
-
-        if(matrix[0][0] == 0){
+        for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                matrix[0][j] = 0;
-            }
-        }
-        if(col0 == 0){
-            for(int i = 0; i < m; i++){
-                matrix[i][0] = 0;
+                if(rows[i] || cols[j]){
+                    matrix[i][j] = 0;
+                }
             }
         }
     }
