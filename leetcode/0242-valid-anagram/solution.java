869e9ed1b0299fb39f957db13bad895f84dd601e
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2025-08-14 17:43:09 +0000

    [LeetCode Sync] Runtime - 1 ms (99.61%), Memory - 43.1 MB (83.05%)

--- leetcode/0242-valid-anagram/solution.java
+++ leetcode/0242-valid-anagram/solution.java
@@ -1,19 +1,20 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        int[] sChar = new int[26];
 
-        if(s.length() != t.length()) return false;
-        
-        int[] count = new int[26];
-
-        for(char c : s.toCharArray()){
-            count[c - 'a']++;
+        for(char sc : s.toCharArray()){
+            sChar[sc - 'a']++;
         }
 
-        for(char c : t.toCharArray()){
-            if(count[c - 'a'] <= 0) return false;
-            count[c - 'a']--;
+        for(char tc : t.toCharArray()){
+            sChar[tc - 'a']--;
         }
 
+        for(int i = 0; i < 26; i++){
+            if(sChar[i] != 0){
+                return false;
+            }
+        }
         return true;
     }
 }
