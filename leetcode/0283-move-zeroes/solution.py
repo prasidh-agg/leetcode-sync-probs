03633660ad2cea103e51600c201308c0dd25111c
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2025-10-19 21:02:32 +0000

    [LeetCode Sync] Runtime - 7 ms (43.14%), Memory - 18.9 MB (49.63%)

---
+++ leetcode/0283-move-zeroes/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        idx = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                # if non zero, move nums[i] to adding index, and change curr indices' value to 0
+                nums[idx], nums[i] = nums[i], nums[idx]
+                idx += 1
+            
