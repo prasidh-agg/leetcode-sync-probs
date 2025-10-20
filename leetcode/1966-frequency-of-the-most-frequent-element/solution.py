3ce6bafd57a3299456e919ce40f4a5f862c3feb0
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2025-10-20 21:11:07 +0000

    [LeetCode Sync] Runtime - 263 ms (81.33%), Memory - 28.4 MB (94.12%)

---
+++ leetcode/1966-frequency-of-the-most-frequent-element/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        nums.sort()
+
+        i = 0
+        n = len(nums)
+        ans = 1
+        curr_sum = 0
+
+        for j in range(n):
+            curr_sum += nums[j]
+
+            while (j - i + 1) * nums[j] - curr_sum > k:
+                curr_sum -= nums[i]
+                i += 1
+            
+            ans = max(ans, j - i + 1)
+
+        return ans
+
