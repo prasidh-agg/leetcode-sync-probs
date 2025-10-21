1163a9478f0055714a7f27a43dc41068c7e19d69
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2025-10-21 18:34:12 +0000

    [LeetCode Sync] Runtime - 7 ms (91.71%), Memory - 28.2 MB (81.00%)

--- leetcode/0209-minimum-size-subarray-sum/solution.py
+++ leetcode/0209-minimum-size-subarray-sum/solution.py
@@ -1,23 +1,18 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         n = len(nums)
-        left = curr_sum = 0
+        # instead of taking a big number, set max to one greater than length of the array
+        ans = n + 1
+        i = 0
+        curr_sum = 0
 
-        # 1 more than max possible len
-        res = n + 1 
-        
-        for right, num in enumerate(nums):
-            curr_sum += num
+        for j in range(n):
+                curr_sum += nums[j]
 
-            while curr_sum >= target and left <= right:
-                res = min(res, right - left + 1)
-                curr_sum -= nums[left]
-                left += 1
-        
-        return 0 if res > n else res
-
+                # as long as curr_sum is greater or equal to target, keep shrinking to find a smaller window
+                while curr_sum >= target:
+                    ans = min(ans, j - i + 1)
+                    curr_sum -= nums[i]
+                    i += 1
             
-
-
-
-
+        return 0 if ans > n else ans
